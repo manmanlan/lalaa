dfdaa2ac3e84e0ed461663a6dafce504148e2a28
maxskolebruker <max2604@skole.vestfoldfylke.no>
2025-02-02 21:17:28 +0100

    aaa

--- max/mavillage.gd
+++ max/mavillage.gd
@@ -1,11 +1,28 @@
 extends Node2D
 
+const PickUp = preload("res://minventory/item/pick_up/pick_up.tscn")
+
 @onready var player_1: CharacterBody2D = $player_1
 @onready var inventory_interface: Control = $UI/InventoryInterface
 
 func _ready() -> void:
 	player_1.toggle_inventory.connect(toggle_inventory_interface)
 	inventory_interface.set_player_inventory_data(player_1.inventory_data)
+	
+	for Node2D in get_tree().get_nodes_in_group("external_inventory"):
+		Node2D.toggle_inventory.connect(toggle_inventory_interface)
 
-func toggle_inventory_interface() -> void:
+func toggle_inventory_interface(external_inventory_owner = null) -> void:
 	inventory_interface.visible = not inventory_interface.visible
+	
+	if external_inventory_owner and inventory_interface.visible:
+		inventory_interface.set_external_inventory(external_inventory_owner)
+	else:
+		inventory_interface.clear_external_inventory()
+
+
+func _on_inventory_interface_2_drop_slot_data(slot_data: SlotData) -> void:
+	var pick_up = PickUp.instantiate()
+	pick_up.slot_data = slot_data
+	pick_up.position = Vector3.UP
+	add_child(pick_up)
--- minventory/inventory/inventory_interface.gd
+++ minventory/inventory/inventory_interface.gd
@@ -1,9 +1,13 @@
 extends Control
 
+signal drop_slot_data(slot_data: SlotData)
+
 var grabbed_slot_data: SlotData
+var external_inventory_owner
 
 @onready var player_inventory: PanelContainer = $PlayerInventory
 @onready var grabbed_slot: PanelContainer = $GrabbedSlot
+@onready var external_inventory: PanelContainer = $ExternalInventory
 
 func _physics_process(delta: float) -> void:
 	if grabbed_slot.visible:
@@ -12,6 +16,25 @@ func _physics_process(delta: float) -> void:
 func set_player_inventory_data(inventory_data: InventoryData) -> void:
 	inventory_data.inventory_interact.connect(on_inventory_interact)
 	player_inventory.set_inventory_data(inventory_data)
+
+func clear_external_inventory() -> void:
+	if external_inventory_owner:
+		var inventory_data = external_inventory_owner.inventory_data
+		
+		inventory_data.inventory_interact.disconnect(on_inventory_interact)
+		external_inventory.clear_inventory_data(inventory_data)
+		
+		external_inventory.hide()
+		external_inventory_owner = null
+	
+func set_external_inventory(_external_inventory_owner) -> void:
+	external_inventory_owner = _external_inventory_owner
+	var inventory_data = external_inventory_owner.inventory_data
+	
+	inventory_data.inventory_interact.connect(on_inventory_interact)
+	external_inventory.set_inventory_data(inventory_data)
+	
+	external_inventory.show()
 
 func on_inventory_interact(inventory_data: InventoryData, 
 		index: int, button: int) -> void:
@@ -34,3 +57,16 @@ func update_grabbed_slot() -> void:
 		grabbed_slot.set_slot_data(grabbed_slot_data)
 	else:
 		grabbed_slot.hide()
+
+
+func _on_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton \
+			and event.is_pressed() \
+			and grabbed_slot_data:
+	
+		match event.button_index:
+			MOUSE_BUTTON_LEFT:
+				drop_slot_data.emit(grabbed_slot_data)
+				grabbed_slot_data = null
+		
+		update_grabbed_slot()
